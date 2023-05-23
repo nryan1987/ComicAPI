baa6d8d2eb29137c0e7756493bdf065cad04cb57
nryan1987 <nryan.brannan@gmail.com>
2023-05-22 21:04:30 -0500

    Update SingleComicResponse.

--- src/main/java/com/api/Comics/models/SingleComicResponse.java
+++ src/main/java/com/api/Comics/models/SingleComicResponse.java
@@ -1,12 +1,32 @@
 package com.api.Comics.models;
 
-import lombok.Data;
+import java.time.LocalDate;
+import java.util.List;
 
-@Data
+import com.api.Comics.entities.NoteEntity;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.ALWAYS)
 public class SingleComicResponse extends Response {	
+	public SingleComicResponse() {}
 	public SingleComicResponse(String message) {
 		super(message);
 	}
+	
+	private int ComicID, volume;
+	
+	private double issue, pricePaid, value;
+	
+	private String title, storyTitle, publisher, picture, condition;
 
-	private ComicModel comicModel;
+	private String recordCreationDate, lastUpdated;
+
+	private LocalDate publicationDate;
+	
+	List<NoteEntity> notes; 
 }
